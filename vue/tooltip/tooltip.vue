706c7ec75bfc4e390a6be46181bdd9eb8b300402
pengjiuyue <67883895+pengjiuyue@users.noreply.github.com>
2023-01-31 14:18:00 +0800

    添加tooltip组件

---
+++ vue/tooltip/tooltip.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-tooltip bottom>
+    <template v-slot:activator="{ on, attrs }">
+      <div v-bind="attrs" v-on="on"></div>
+    </template>
+    <span>Not supported on V2 devices.</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {},
+
+  methods: {},
+};
+</script>
+
+<style scoped lang="scss"></style>
